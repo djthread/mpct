0cf7df5984d11ccbf05291994d478cede6f2ee38
thread <thethread@gmail.com>
2012-01-23 22:19:49 -0500

    jQuery Mobile web interface \o/

--- web/index.php
+++ web/index.php
@@ -52,30 +52,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     if (preg_match('/^(.+):(\d+)$/', $m['host'], $matches)) {
-        $params = "--host {$matches[1]} --port {$matches[2]} $params";
+        $params = "-h {$matches[1]} -p {$matches[2]} $params";
     } else {
-        $params = "--host {$m['host']} $params";
+        $params = "-h {$m['host']} $params";
     }
 
-    // We'll just add the count parameter... even if it's irrelevant (like -ta)
-    if (!isset($m['count']) || !ctype_digit($m['count']) || $m == 0 || $m > 99) {
-        $count = 1;
-    } else $count = $m['count'];
-    $params .= ' -c ' . $m['count'];
+    if ($m['action'] != 'thisAlbum') {
+        if (!isset($m['count']) || !ctype_digit($m['count']) || $m == 0 || $m > 99) {
+            $count = 1;
+        } else $count = $m['count'];
+        $params .= ' -c ' . $m['count'];
+    }
 
-    if (isset($m['BT']) && preg_match('/^[a-z]{2}$/', $m['BT'])) {
-        $params .= ' --by-toplevel ' . $m['BT'];
+    if (isset($m['BT']) && $m['BT'] != '00' && preg_match('/^[a-z0-9]{2}$/', $m['BT'])) {
+        $params .= ' -bt ' . $m['BT'];
     }
 
     if (isset($m['append']) && $m['append']) {
-        $params .= ' --append';
+        $params .= ' -a';
     }
 
     // save it for later !
     $_SESSION['m'] = $m;
 
+    $dispcmd = preg_replace('/^.*\//', '.../', $mpct) . " $params";
+    echo "\$ $dispcmd\n";
+
     $cmd = "$mpct $params";
-    echo "\$ $cmd\n";
     echo `$cmd`;
     die();
 }
@@ -83,7 +86,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 $m = isset($_SESSION['m']) ? $_SESSION['m'] : array(
     'host'   => $hosts[0],
     'action' => 'randomTracks',
-    'useBT'  => false,
     'BT'     => null,
     'count'  => 5,
     'append' => false
@@ -93,57 +95,57 @@ $m = isset($_SESSION['m']) ? $_SESSION['m'] : array(
 <html>
 <head>
 <title>mpct</title>
-<link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
 <script type="text/javascript" src="mpct.js"></script>
+<style type="text/css">
+div#message { font-size: 70%; }
+</style>
 </head>
 <body>
+<div data-role="page" data-theme="a" data-title="mpct">
+
+<div data-role="header">
+    <h1>mpct</h1>
+</div>
+
+<div data-role="content">
 <form id="m_form" method="post">
-<div class="group">
-    <div class="field">
-    <?php if (count($hosts) > 1) { ?>
+    <label for="m_host" style="display:none">Host</label>
     <select id="m_host" name="m[host]">
     <?php
     foreach ($hosts as $host) {
-        echo "\t<option "
+        echo "\t<option"
             . ($m['host'] == $host ? ' selected' : '')
             .">$host</option>\n";
     }
     ?>
     </select>
-    <?php } else { ?>
-    <label><?php echo $hosts[0]; ?></label>
-    <input type="hidden" name="m[host]" value="<?= $hosts[0] ?>">
-    <?php } ?>
-    </div>
-</div>
-<div class="group">
-    <div class="field">
+
+    <fieldset data-role="controlgroup">
     <input type="radio" id="m_action_randomTracks" name="m[action]" value="randomTracks"<?=
         $m['action'] == 'randomTracks' ? ' checked' : '' ?>>
     <label for="m_action_randomTracks">Random Tracks</label>
-    </div>
-    <div class="field">
+
     <input type="radio" id="m_action_randomAlbums" name="m[action]" value="randomAlbums"<?=
         $m['action'] == 'randomAlbums' ? ' checked' : '' ?>>
     <label for="m_action_randomAlbums">Random Albums</label>
-    </div>
-    <div class="field">
+
     <input type="radio" id="m_action_thisAlbum" name="m[action]" value="thisAlbum"<?=
         $m['action'] == 'thisAlbum' ? ' checked' : '' ?>>
     <label for="m_action_thisAlbum">This Album</label>
-    </div>
-</div>
-<div class="group">
-    <div class="field">
-    <input type="checkbox" id="m_useBT" name="m[useBT]"<?php
-        echo $m['useBT'] ? ' checked' : ''; ?>>
+    </fieldset>
+
     <select id="m_BT" name="m[BT]">
     <?php
-        $file = `$mpct --get-toplevels`;
-        foreach (split("\n", trim(`$mpct --get-toplevels`)) as $line) {
-            if (!preg_match('/^([a-z]{2}) (.+)$/', $line, $matches)) {
+        $lines = array_merge(
+            array('00 All Genres'),
+            split("\n", trim(`$mpct --get-toplevels`))
+        );
+        foreach ($lines as $line) {
+            if (!preg_match('/^([a-z0-9]{2}) (.+)$/', $line, $matches)) {
                 continue;
             }
             echo '<option value="' . $matches[1] . '"'
@@ -153,28 +155,18 @@ $m = isset($_SESSION['m']) ? $_SESSION['m'] : array(
         }
     ?>
     </select>
-    </div>
-</div>
-<div class="group">
-    <div class="field">
-    <input type="text" id="m_count" name="m[count]" size="3" maxlength="2" value="<?= $m['count'] ?>">
-    <label for="m_count">Count</label>
-    </div>
-</div>
-<div class="group">
-    <div class="field">
+
+    <label for="m_count" style="display:none">Count</label>
+    <input type="range" name="m[count]" id="m_count" value="<?=$m['count'] ?>" min="1" max="20"  />
+
     <input type="checkbox" id="m_append" name="m[append]"<?= $m['append'] ? ' checked' : '' ?>>
     <label for="m_append">Append</label>
-    </div>
-</div>
-<div class="group">
-    <div class="field">
+
     <input type="submit" id="m_submit" value="  Go  ">
     <div id="message"></div>
-    </div>
+</form>
 </div>
-<?php
 
-?>
+</div>
 </body>
 </html>
--- web/mpct.js
+++ web/mpct.js
@@ -8,25 +8,18 @@ $(document).ready(function() {
         });
     });
     var twiddle = function(e) {
-        var b = $('#m_useBT');
         if ($('#m_action_randomTracks').attr('checked')
          || $('#m_action_randomAlbums').attr('checked')
         ) {
-                        b.prop('disabled', false);
             $('#m_count').prop('disabled', false);
-        } else {
-                        b.prop('disabled', true);
-            $('#m_count').prop('disabled', true);
-        }
-        if (b.attr('disabled') || !b.attr('checked')) {
-            $('#m_BT').prop('disabled', true);
-        } else {
             $('#m_BT').prop('disabled', false);
+        } else {
+            $('#m_count').prop('disabled', true);
+            $('#m_BT').prop('disabled', true);
         }
     };
     $('#m_action_randomTracks').click(twiddle);
     $('#m_action_randomAlbums').click(twiddle);
     $('#m_action_thisAlbum').click(twiddle);
-    $('#m_useBT').click(twiddle);
     twiddle();
 });
